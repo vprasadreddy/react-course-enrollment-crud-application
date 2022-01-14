eed9ebf17520686a5452dcebfeb44900a1bf80fe
vprasadreddy <prasad.reddy0708@gmail.com>
2022-01-13 21:31:06 -0500

    added jwt authentication

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,14 +1,184 @@
 const { application } = require("express");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
-const { body, validationResult } = require("express-validator");
+const { body, check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const authenticate = require("../middlewares/authenticate");
+
 router.get("/", (req, res) => {
   res.send("response from user router");
 });
 
-router.get("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-});
+router.post(
+  "/register",
+  [
+    check("name").notEmpty().withMessage("name is required"),
+    check("email")
+      .notEmpty()
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email is not valid email"),
+    check("password").notEmpty().withMessage("password is required"),
+  ],
+  async (req, res) => {
+    const { name, email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      let user = await User.findOne({
+        email,
+      });
+      if (user) {
+        return res.status(400).json({ "error message": "user already exists" });
+      }
+      //encrypt the password
+      let salt = await bcrypt.genSalt(10);
+      let encryptedPassword = await bcrypt.hash(password, salt);
+      let newUser = new User({
+        name,
+        email,
+        password: encryptedPassword,
+      });
+      let savedUser = await newUser.save();
+      return res
+        .status(200)
+        .json({ message: "user registered successfully", user: savedUser });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    check("email")
+      .notEmpty()
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email is not valid email"),
+    check("password").notEmpty().withMessage("password is required"),
+  ],
+  async (req, res) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      let user = await User.findOne({
+        email,
+      });
+      if (user) {
+        //compare password
+        let isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (isPasswordMatch) {
+          //create JWT token
+          let payload = {
+            user: {
+              id: user.id,
+              name: user.name,
+              isAdmin: user.isAdmin,
+            },
+          };
+
+          jwt.sign(payload, process.env.JWT_SECRET_KEY, (error, token) => {
+            if (error) throw error;
+            res.status(200).json({
+              message: "Login success",
+              token,
+            });
+          });
+        } else {
+          return res.status(400).json({ errormessage: "Invalid credentials" });
+        }
+      } else {
+        return res.status(400).json({ errormessage: "User not found" });
+      }
+    }
+  }
+);
+
+//update User
+// { new: true } should be passed as an option to get the updated product as response.
+router.put(
+  "/updateuser",
+  [
+    check("name").notEmpty().withMessage("name is required"),
+    check("email")
+      .notEmpty()
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email is not valid email"),
+  ],
+  (req, res) => {
+    let updatedUser = {
+      name: req.body.name,
+      email: req.body.email,
+    };
+    User.findByIdAndUpdate(
+      email,
+      { $set: updatedUser },
+      { new: true },
+      (err, document) => {
+        if (err) {
+          res.status(400).json({ err: err });
+        } else {
+          res
+            .status(200)
+            .json({ msg: "User updated successfully", User: document });
+        }
+      }
+    );
+  }
+);
+
+router.delete(
+  "/deleteuser",
+  authenticate,
+  [
+    check("email")
+      .notEmpty()
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email is not valid email"),
+  ],
+  async (req, res) => {
+    const { email } = req.body;
+    let isAdmin = req.user.isAdmin;
+    console.log(isAdmin);
+    if (isAdmin) {
+      if (email) {
+        let user = await User.findOne({
+          email,
+        });
+        if (user) {
+          await User.findOneAndDelete({ email }, (err, document) => {
+            if (err) {
+              res.status(400).json({ err: err });
+            } else {
+              res.status(200).json({
+                msg: "User deleted successfully",
+                user: document,
+              });
+            }
+          });
+        } else {
+          return res.status(400).json({
+            errormessage: `User not found with email: ${email}`,
+          });
+        }
+      } else {
+        return res
+          .status(400)
+          .json({ errormessage: `User email cannot be empty` });
+      }
+    } else {
+      return res
+        .status(401)
+        .json({ errormessage: `User is not authorized to delete user` });
+    }
+  }
+);
 
 module.exports = router;
