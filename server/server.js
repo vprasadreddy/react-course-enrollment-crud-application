23561b8569ff9e6b6277c2fd3e6684d1b874409a
vprasadreddy <prasad.reddy0708@gmail.com>
2022-01-20 18:32:48 -0500

    initial deployment

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 const router = express.Router();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 //configure cors
 app.use(cors());
@@ -46,6 +47,9 @@ app.listen(port || 3000, hostname, () => {
 });
 
 //deployment
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
 }
