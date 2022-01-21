69ba8ae3dba47b764639bfd7fdba6d584f64320f
vprasadreddy <prasad.reddy0708@gmail.com>
2022-01-20 20:13:49 -0500

    initial deployment

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.listen(port || 3000, () => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "client", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
