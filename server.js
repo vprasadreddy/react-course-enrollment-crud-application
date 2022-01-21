9a7f0f6419056a011da06e4751d519c82723fa25
vprasadreddy <prasad.reddy0708@gmail.com>
2022-01-20 22:26:19 -0500

    make it better

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.listen(port || 3000, () => {
 //deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
